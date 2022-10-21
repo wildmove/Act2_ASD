dba15996b2e40305549c96b680fc307c2d7f8130
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-10-21 17:19:02 +0700

    Menambahkan method swap

--- QuickSort_090/Program.cs
+++ QuickSort_090/Program.cs
@@ -36,6 +36,15 @@ namespace QuickSort
                 arr[i] = Int32.Parse(s1);
             }
         }
-        
+        //swaps the element at index x with the element at index y
+        void swap(int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+
     }
 }
