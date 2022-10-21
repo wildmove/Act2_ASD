930c60f4562d6e0ce2bfed09317416f9d7a1607a
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-10-21 17:12:57 +0700

    menambahkan method input

--- QuickSort_090/Program.cs
+++ QuickSort_090/Program.cs
@@ -4,12 +4,38 @@ namespace QuickSort
 {
     class Program
     {
-           //array of integers to hold values
-           private int[] arr = new int[20];
-           private int cmp_mount = 0; // number of comparison
-           private int mov_sort = 0; // number of data movements
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_mount = 0; // number of comparison
+        private int mov_sort = 0; // number of data movements
 
-           // Number of elements in array
-           private int n;
+        // Number of elements in array
+        private int n;
+
+        void input ()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 0)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements");
+            }
+            Console.WriteLine("\n==================");
+            Console.WriteLine("Enter array elements");
+            Console.WriteLine("\n================--");
+
+            //get array elements
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+        
     }
 }
