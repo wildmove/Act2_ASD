1d80c1b9a3458d38ca6e23d23708c73df96467a3
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-10-21 16:57:40 +0700

    Mendefinisikan tipe data yang dibutuhkan

--- QuickSort_090/Program.cs
+++ QuickSort_090/Program.cs
@@ -4,6 +4,12 @@ namespace QuickSort
 {
     class Program
     {
+           //array of integers to hold values
+           private int[] arr = new int[20];
+           private int cmp_mount = 0; // number of comparison
+           private int mov_sort = 0; // number of data movements
 
+           // Number of elements in array
+           private int n;
     }
 }
